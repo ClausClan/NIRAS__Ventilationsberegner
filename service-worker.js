175dd1e1188b2520008daa250dd221f6c03941e9
ClausClan <160112433+ClausClan@users.noreply.github.com>
2025-09-23 16:28:10 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'ventilation-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return from cache
+        }
+        return fetch(event.request); // Fetch from network
+      }
+    )
+  );
+});
